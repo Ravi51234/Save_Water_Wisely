8ce3bbeb4d3d1c3ca926a02e699edb2ff9fe6b12
Ravi51234 <140091754+Ravi51234@users.noreply.github.com>
2023-07-25 17:01:38 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,30 @@
+﻿function bodyload()
+{
+document.getElementById("activity1").style.backgroundColor="aqua";
+document.getElementById("activity1").style.borderRadius="50px";
+document.getElementById("activity1").style.border="30px normal black"; 
+document.getElementById("activity1").style.padding="10px";
+  
+document.getElementById("activity2").style.backgroundColor="aqua";
+document.getElementById("activity2").style.borderRadius="50px";
+document.getElementById("activity2").style.border="30px normal black"; 
+document.getElementById("activity2").style.padding="10px";
+  
+document.getElementById("activity3").style.backgroundColor="aqua";
+document.getElementById("activity3").style.borderRadius="50px";
+document.getElementById("activity3").style.border="30px normal black"; 
+document.getElementById("activity3").style.padding="10px";
+  
+document.getElementById("activity4").style.backgroundColor="aqua";
+document.getElementById("activity4").style.borderRadius="50px";
+document.getElementById("activity4").style.border="30px normal black"; 
+document.getElementById("activity4").style.padding="10px";
+  
+document.getElementById("activity5").style.backgroundColor="aqua";
+document.getElementById("activity5").style.borderRadius="50px";
+document.getElementById("activity5").style.border="30px normal black"; 
+document.getElementById("activity5").style.padding="10px";
+}
+
+
+
